f450bf1567f2fb7c7cb3f3d5f079ec77b5b9e43b
guptaharsh686 <56353320+guptaharsh686@users.noreply.github.com>
2023-11-11 17:11:03 +0530

    Create DeleteFirstNodeFromLinkedList.cs

---
+++ Singly Linked List/DeleteFirstNodeFromLinkedList.cs
@@ -0,0 +1,62 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        Node head = new(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        //int value = 9;
+
+        //Console.WriteLine($"\nBefore insert of value {value}");
+        printLinkedList(head);
+
+
+        head = DeleteFirstNode(head);
+        Console.WriteLine();
+        //Console.WriteLine($"\nAfter insert of value {value}");
+
+        printLinkedList(head);
+        
+    }
+
+    private static Node DeleteFirstNode(Node head)
+    {
+        if(head == null)
+        {
+            return null;
+        }
+
+        //no need to worry about memory deallocation
+
+        return head.next;
+        
+    }
+
+    private static void printLinkedList(Node head)
+    {
+        Node curr = head;
+        while (curr != null)
+        {
+            Console.Write(curr.data + " ");
+            curr = curr.next;
+        }
+    }
+}
+
+public class Node
+{
+    public int data;
+    public Node? next;
+    public Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+
+
