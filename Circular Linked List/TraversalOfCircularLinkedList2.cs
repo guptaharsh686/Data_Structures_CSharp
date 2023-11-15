d4ce3f7fe9a7095d6ea83e7092d68acff2f2859f
guptaharsh686 <56353320+guptaharsh686@users.noreply.github.com>
2023-11-15 16:44:20 +0530

    Create TraversalOfCircularLinkedList2.cs

---
+++ Circular Linked List/TraversalOfCircularLinkedList2.cs
@@ -0,0 +1,58 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+        head.next.next.next.next.next = head;
+
+
+        //int data = 55;
+
+        //Console.WriteLine($"\nBefore insert of value {data}");
+        printLinkedList(head);
+
+
+        //head = RemoveDuplicateFromSortedList(head);
+        //Console.WriteLine($"\n");
+
+        //printLinkedList(head);
+        
+    }
+
+    private static void printLinkedList(Node head)
+    {
+        if(head == null)
+        {
+            return;
+        }
+        Node curr = head;
+        do
+        {
+            Console.Write($"{curr.data} ");
+            curr = curr.next;
+        } while (curr != head);
+    }
+}
+
+
+
+    public class Node
+    {
+        public int data;
+        public Node? next;
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+
+
+
