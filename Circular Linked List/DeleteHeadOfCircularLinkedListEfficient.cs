cb88bb63c8a3a1b81269b37d8103d116a3d1a0da
guptaharsh686 <56353320+guptaharsh686@users.noreply.github.com>
2023-12-08 09:26:12 +0530

    Create DeleteHeadOfCircularLinkedListEfficient.cs

---
+++ Circular Linked List/DeleteHeadOfCircularLinkedListEfficient.cs
@@ -0,0 +1,72 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        Node head = new Node(10);
+        //head.next = head;
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+        head.next.next.next.next.next = head;
+
+
+        //int data = 55;
+
+        //Console.WriteLine($"\nBefore insert of value {data}");
+        printLinkedList(head);
+
+
+        head = DeleteHeadOfCircularLinkedListEfficient(head);
+        Console.WriteLine($"\n");
+
+        printLinkedList(head);
+        
+    }
+
+    private static Node? DeleteHeadOfCircularLinkedListEfficient(Node head)
+    {
+        if(head == null || head.next == head)
+        {
+            return null;
+        }
+
+        head.data = head.next.data;
+        head.next = head.next.next;
+
+        return head;
+    }
+
+    private static void printLinkedList(Node head)
+    {
+        if(head == null)
+        {
+            return;
+        }
+        Node curr = head;
+        do
+        {
+            Console.Write($"{curr.data} ");
+            curr = curr.next;
+        } while (curr != head);
+    }
+}
+
+
+
+    public class Node
+    {
+        public int data;
+        public Node? next;
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+
+
+
